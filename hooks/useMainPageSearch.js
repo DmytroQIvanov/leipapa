cbd5488e0bebda01aabdf02c12fd833b21ae5ef1
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2022-06-14 18:44:50 +0300

    added:initial values

--- hooks/useMainPageSearch.js
+++ hooks/useMainPageSearch.js
@@ -25,6 +25,18 @@ const UseMainPageSearch = ({ selectedCountry }) => {
       });
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.company-information.service.gov.uk/search/companies?q=hold",
+        {
+          headers: {
+            Authorization: "Bearer a0a093ec-5597-40b6-94ec-967e8016c28a",
+          },
+        }
+      )
+      .then((elem) => console.log(elem));
+  }, []);
   // const [companyText, setCompanyText] = useState("");
   const [statesList, setStatesList] = useState([]);
   const [companiesList, setCompaniesList] = useState([]);
