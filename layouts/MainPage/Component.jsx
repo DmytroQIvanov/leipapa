951a9f0c2e8185ec18105218994e071804b456a0
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2022-06-21 13:28:07 +0300

    some changes....

--- layouts/MainPage/Component.jsx
+++ layouts/MainPage/Component.jsx
@@ -17,6 +17,23 @@ import HeadquartersAddressBlock from "./Components/HeadquartersAddressBlock";
 import DirectParentCompanySubBlock from "./Components/DirectParentCompanySubBlock";
 import UltimateParentCompany from "./Components/UltimateParentCompany";
 
+import Backdrop from "@mui/material/Backdrop";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "50vw",
+  borderRadius: "8px",
+  bgcolor: "background.paper",
+  // border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const Component = ({ data }) => {
   //HOOKS
   const { values, touched, errors, handleSubmit, handleChange, setFieldValue } =
@@ -37,6 +54,10 @@ const Component = ({ data }) => {
 
   const [inputCountry, setInputCountry] = useState("");
 
+  const [open, setOpen] = React.useState(false);
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
+
   //USE-EFFECTS
   useEffect(() => {
     if (
@@ -49,6 +70,10 @@ const Component = ({ data }) => {
       setStateVisibly(false);
     }
   }, [values.country]);
+
+  useEffect(() => {
+    values.company && values.company.company_name && handleOpenModal();
+  }, [values.company]);
 
   //FUNCTIONS
   const handleClose = () => {
@@ -126,27 +151,25 @@ const Component = ({ data }) => {
             )}
           </Box>
         </div>
-
         <SelectableBlock
           handleChange={(elem) => setFieldValue("headquartesAddress", elem)}
           value={values.headquartesAddress}
           label={"Headquartes address is identical to legal address?"}
         />
-
-        <Box sx={{ display: "flex", flexDirection: "column", mb: "15px" }}>
-          <CustomRadioButtonsGroup
-            title={"Entity type"}
-            value={values.typeOfEmployment}
-            handleChange={(e, value) => {
-              setFieldValue("typeOfEmployment", value);
-            }}
-            buttonsArray={[
-              { label: "Private person", value: "private" },
-              { label: "Company", value: "company" },
-            ]}
-          />
-          {values.typeOfEmployment == "company" && <input type={"file"} />}
-        </Box>
+        {/*<Box sx={{ display: "flex", flexDirection: "column", mb: "15px" }}>*/}
+        {/*  <CustomRadioButtonsGroup*/}
+        {/*    title={"Entity type"}*/}
+        {/*    value={values.typeOfEmployment}*/}
+        {/*    handleChange={(e, value) => {*/}
+        {/*      setFieldValue("typeOfEmployment", value);*/}
+        {/*    }}*/}
+        {/*    buttonsArray={[*/}
+        {/*      { label: "Private person", value: "private" },*/}
+        {/*      { label: "Company", value: "company" },*/}
+        {/*    ]}*/}
+        {/*  />*/}
+        {/*  {values.typeOfEmployment == "company" && <input type={"file"} />}*/}
+        {/*</Box>*/}
         <Box className={styles.mainPageContainer__secondBlock}>
           {/*---ENTITY ADDRESS BLOCK---*/}
           <EntityAddressBlock {...{ handleChange, values, touched, errors }} />
@@ -161,7 +184,96 @@ const Component = ({ data }) => {
             }}
           />
         </Box>
+        <div>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleCloseModal}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <Box sx={style}>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  sx={{ fontWeight: "700" }}
+                >
+                  Dear customer, there is a possible duplicate match between
+                  your application and Global Legal Entity (GLEIF) database.
+                  Please check the information below to confirm if this is your
+                  company or not.
+                </Typography>
+                <Box sx={{ ml: "35px", my: "10px" }}>
+                  <div>
+                    <b>LEI:</b>
+                    {values.companyNumber}
+                  </div>
+                  <div>
+                    <b>Company:</b>
+                    {values.company?.name}
+                  </div>
+                  <div>
+                    <b>Reg nr:</b>
+                    {values.companyNumber}
+                  </div>
+                  <div>
+                    <b>Next renewal date:</b>
+                    {values.companyNumber}
+                  </div>
+                  <div>
+                    <b>Status:</b>
+                    {values.companyNumber}
+                  </div>
+                </Box>
+                <Typography
+                  id="transition-modal-description"
+                  sx={{ mt: 2, fontWeight: "700" }}
+                >
+                  In case this is your company information above, clock YES to
+                  proceed to LEI renewal application. If you are sure this is
+                  not your entity, click NO and proceed with new LEI
+                  application.
+                </Typography>
 
+                <Box sx={{ display: "flex", mt: "15px" }}>
+                  <Button
+                    variant={"contained"}
+                    color={"error"}
+                    sx={{
+                      width: "40%",
+                      lineHeight: "15px",
+                      fontSize: "14px",
+                      color: "black",
+                      fontWeight: "600",
+                    }}
+                    onClick={() => handleCloseModal()}
+                  >
+                    No - proceed with new LEI application.
+                  </Button>
+                  <Button
+                    variant={"contained"}
+                    color={"success"}
+                    sx={{
+                      m: "auto 0px auto auto",
+                      width: "40%",
+                      lineHeight: "15px",
+                      color: "black",
+                      fontWeight: "600",
+                    }}
+                    onClick={() => handleCloseModal()}
+                  >
+                    Yes, it is my company. Proceed with LEI Renewal
+                  </Button>
+                </Box>
+              </Box>
+            </Fade>
+          </Modal>
+        </div>
         <SelectableBlock
           handleChange={(elem) => setFieldValue("companyOwned", elem)}
           label={"Is the company owned by another company? (>50%) *"}
