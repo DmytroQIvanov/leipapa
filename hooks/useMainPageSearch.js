4f5c4ec0292f897349b26a86733d681926c8a518
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2022-07-01 16:05:00 +0300

    some changes....

--- hooks/useMainPageSearch.js
+++ hooks/useMainPageSearch.js
@@ -95,7 +95,7 @@ const UseMainPageSearch = ({ values, setFieldValue }) => {
       .get("https://apistaging.rapidlei.com/v1/auth/token", {
         headers: {
           // "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Origin": "https://leipapa.vercel.app/",
+          // "Access-Control-Allow-Origin": "https://leipapa.vercel.app/",
           // "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
           // "Access-Control-Request-Headers": "Authorization, Content-Type",
           "Content-Type": "application/json",
